a2ebe480bf3c17001df604652b549b1aba3eaa08
Lemi <tomys.leon@gmail.com>
2026-02-22 16:55:05 +0100

    Struktura projektu

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-800 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-emerald-600 shadow-lg p-6 bg-white rounded-2xl">
+        Tailwind works
+        
+      </h1>
+    </div>
+  )
+}
+export default App
+
