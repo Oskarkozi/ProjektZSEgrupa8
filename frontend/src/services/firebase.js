0ac35d770662bbe3eeb0f4e28e835ec7dfa4f7ee
Lemi <tomys.leon@gmail.com>
2026-03-01 16:43:36 +0100

    Poprawa kluczy do bazy

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -9,13 +9,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyApUTKNsiPSLP1TkhapQgD5wXZSriZZhtc",
-  authDomain: "bankx-zespol8.firebaseapp.com",
-  projectId: "bankx-zespol8",
-  storageBucket: "bankx-zespol8.firebasestorage.app",
-  messagingSenderId: "321558989889",
-  appId: "1:321558989889:web:f800a100fe6f84a04b4ac0",
-  measurementId: "G-ZPLSYLR7M4"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
